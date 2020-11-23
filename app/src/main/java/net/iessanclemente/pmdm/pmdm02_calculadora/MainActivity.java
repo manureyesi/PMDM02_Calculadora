1d3258c24c497f82c6bd82ee8e31e6ebbc33aa83
manureyesi <manureyesi@outlook.es>
2020-11-23 17:09:03 +0100

    Primer commit

---
+++ app/src/main/java/net/iessanclemente/pmdm/pmdm02_calculadora/MainActivity.java
@@ -0,0 +1,62 @@
+package net.iessanclemente.pmdm.pmdm02_calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import net.iessanclemente.pmdm.pmdm02_calculadora.service.CalculadoraPrimos;
+
+import static net.iessanclemente.pmdm.pmdm02_calculadora.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botonCalcular;
+    private EditText numeroCalcular;
+    private TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+
+        this.botonCalcular = findViewById(id.buttonCalcular);
+
+        this.numeroCalcular = findViewById(id.numeroCalcular);
+
+        this.resultado = findViewById(id.textView2);
+
+        // Function para el click
+        this.botonCalcular.setOnClickListener(this::onClick);
+
+    }
+
+    private void onClick(View view) {
+
+        String textoNumeroCalcular = this.numeroCalcular.getText().toString();
+        if (!textoNumeroCalcular.isEmpty()) {
+
+            Integer numeroCalcular = null;
+            // Transformar Número
+            try {
+                numeroCalcular = Integer.valueOf(textoNumeroCalcular);
+            } catch (NumberFormatException e) {
+
+            }
+
+            Integer resultado = null;
+            if (numeroCalcular != null) {
+                resultado = CalculadoraPrimos.calcularPrimo(numeroCalcular);
+
+
+                this.resultado.setText(String.format(
+                        getString(string.app_texto_respuesta_calcular), numeroCalcular.toString(), resultado.toString()));
+
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/net/iessanclemente/pmdm/pmdm02_calculadora/service/CalculadoraPrimos.java
@@ -0,0 +1,68 @@
+package net.iessanclemente.pmdm.pmdm02_calculadora.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculadoraPrimos {
+
+    private static Map<Integer, Integer> mapNumerosCalculados = new HashMap<>();
+
+    public static Integer calcularPrimo (final Integer numero) {
+
+        Integer numeroPrimo = null;
+
+        // Comprobamos si o número esta no Map calculado
+        if (mapNumerosCalculados.containsKey(numero)) {
+            numeroPrimo = mapNumerosCalculados.get(numero);
+        } else {
+            // Como non esta no Mapa temolo que calcular
+            int contadorPrimos = 0;
+            numeroPrimo = 1;
+
+            // Como se gardan todos os números primos buscamos o maior gardado
+            if (mapNumerosCalculados.containsKey(mapNumerosCalculados.size())) {
+                contadorPrimos = mapNumerosCalculados.size();
+                numeroPrimo = mapNumerosCalculados.get(contadorPrimos);
+            }
+
+            while(contadorPrimos<numero) {
+                numeroPrimo++;
+                System.out.println(String.format("Comprobando si el Número %s es primo", numeroPrimo));
+                if (esPrimo(numeroPrimo)) {
+                    contadorPrimos++;
+                    // Gardamos todos os numeros primos xa calculados
+                    mapNumerosCalculados.put(contadorPrimos, numeroPrimo);
+                }
+            }
+
+        }
+
+        return numeroPrimo;
+    }
+
+    /**
+     * Comprobar si un número es Primo
+     * @param numero
+     * @return
+     */
+    private static boolean esPrimo (final Integer numero) {
+
+        boolean esPrimo = Boolean.FALSE;
+
+        int contador = 0;
+        for (int i = 2; i < numero; i++) {
+            if (numero%i==0) {
+                contador++;
+                break;
+            }
+        }
+
+        // Contador si es un numero primo
+        if (contador == 0) {
+            esPrimo = Boolean.TRUE;
+        }
+
+        return esPrimo;
+    }
+
+}
